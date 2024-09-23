987a617b7aed1aed875791743a2b87c0a8003b00
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-23 09:40:01 -0300

    Adicao do metado get_events: Busca os ventos do usuario pelo username e retorna os dados !

--- github_user_activity/git_api.py
+++ github_user_activity/git_api.py
@@ -6,3 +6,30 @@ import json
 class USER:
     def __init__(self) -> None:
         self.api_url_git = "api.github.com"
+
+    def get_events(self, username):
+        # conexao
+        connect = http.client.HTTPSConnection(self.api_url_git)
+
+        # hearders
+        headers = {"User-Agent": "Python Script"}
+
+        # requisicao
+        connect.request(
+            "GET", f"https://api.github.com/users/{username}/events", headers=headers
+        )
+
+        # dados
+        response = connect.getresponse()
+
+        if response.status == 200:
+            data = response.read()
+            events = json.loads(data.decode())
+
+            # retorna dados
+            return events
+
+        else:
+            print(
+                f"\nRequest failed: {response.status} {response.reason}\ncheck if the username was typed wrong \n"
+            )
