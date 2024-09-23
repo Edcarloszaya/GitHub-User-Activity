da6a2740f5c368a07545188071d81a20b4ec47e7
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-23 14:00:12 -0300

    Configuracao do argpase pra usar o username como entrada. Estancia a classe User resposavel pela busca de eventos ,depois exibi o resultado se tiver!

--- github_user_activity/github-activity.py
+++ github_user_activity/github-activity.py
@@ -1,3 +1,20 @@
+import argparse
+from github_user_activity.git_api import USER
 
-           
+parser = argparse.ArgumentParser(description="Github-activity")
+parser.add_argument("Username", type=str, help="Username from the git  ")
+
+args = parser.parse_args()
+user_name = args.Username
+
+# instacia a classe 
+user_api = USER()
+
+if user_name:
+    events = user_api.get_events(user_name)
+    user_api.show_events(events)
+
+
+
+
 
