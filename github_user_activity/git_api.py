88bada0f7e315b6ab476a73970243a61da484194
Edcarlos <Edcarlos.molequedoido@gmail.com>
2024-09-23 10:11:13 -0300

    Adicao show_events : Recebe dados da requisicao se tiver como paramento verifica se tem dados itera e mostra os eventos

--- github_user_activity/git_api.py
+++ github_user_activity/git_api.py
@@ -33,3 +33,22 @@ class USER:
             print(
                 f"\nRequest failed: {response.status} {response.reason}\ncheck if the username was typed wrong \n"
             )
+
+
+    def show_events(self,events):
+         if events:
+         
+            for event in events:
+                event_type = event['type']
+                repo_name = event['repo']['name']
+
+                if event_type == "PushEvent":
+                    commits = len(event['payload']['commits'])
+                    print(f"- Pushed {commits} commits to {repo_name}")
+                
+                elif event_type == "IssuesEvent":
+                    action = event['payload']['action']
+                    print(f"- Opened a new issue in {repo_name}")
+                
+                elif event_type == "WatchEvent":
+                    print(f"- Starred {repo_name}")
